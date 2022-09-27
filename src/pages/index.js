d1344af40a27bdc9bde1f4e2571b6fc0b1e30aec
Nardika Kurniawan <nardikakurniawan20@gmail.com>
2022-09-27 14:19:11 +0700

    Component Header, fonts, svg, dan setup _document nextjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+
+import Header from "components/Header";
 
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title>Jadoo</title>
-        <meta name='description' content='Jadoo App Travel' />
+        <meta name="description" content="Jadoo App Travel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="bg-gray-900 text-white">Hello World</div>
+      <div className="absolute z-10 w-full py-12">
+        <Header />
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Poppins, sans-serif",
+        serif: "Volkhov, serif",
+      },
+      colors: {
+        "accent-1": "#F1A501",
+      },
+    },
   },
   plugins: [],
-}
+};
